d8958c0fcd1580308e025bd6c9c978f687fbd48d
Aleksey <a.e.sokolkov@gmail.com>
2025-12-20 18:46:24 +0500

    feat: version

---
+++ go.mod
@@ -0,0 +1,14 @@
+module github.com/sangrita-tech/golang-pkg
+
+go 1.23.3
+
+require github.com/stretchr/testify v1.11.1
+
+require (
+	github.com/alexliesenfeld/health v0.8.1 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
+	go.uber.org/zap v1.27.1 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ pkg/version/version.go
@@ -0,0 +1,35 @@
+package version
+
+import (
+	"time"
+)
+
+var (
+	Version = "dev"
+	Commit  = "unknown"
+	Date    = "unknown"
+)
+
+type BuildInfo struct {
+	Version string     `json:"version"`
+	Commit  *string    `json:"commit"`
+	Date    *time.Time `json:"date"`
+}
+
+func GetInfo() BuildInfo {
+	b := BuildInfo{
+		Version: Version,
+	}
+
+	if Commit != "" && Commit != "unknown" {
+		b.Commit = &Commit
+	}
+
+	if Date != "" && Date != "unknown" {
+		if t, err := time.Parse(time.RFC3339, Date); err == nil {
+			b.Date = &t
+		}
+	}
+
+	return b
+}
